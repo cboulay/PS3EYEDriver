75e74a2bd280dbab86c89b3199616f1b5203dff9
Chadwick Boulay <chadwick.boulay@gmail.com>
2015-03-09 16:44:14 -0400

    Necessary for VS 2013.

--- src/ps3eye.h
+++ src/ps3eye.h
@@ -17,9 +17,9 @@
 #include <stdint.h>
 
 #if defined(DEBUG)
-#define debug(x...) fprintf(stdout,x)
+#define debug(x,...) fprintf(stdout,x)
 #else
-#define debug(x...) 
+#define debug(x,...) 
 #endif
 
 
